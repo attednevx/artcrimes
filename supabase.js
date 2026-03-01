8c4976f76cadd046f3782c152195080b2509da9b
Attednev <ernadagovic04@gmail.com>
2026-03-01 20:26:10 +0100

    Update Supabase anon key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://nxzmegxvgzllhkvkaczt.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloanhudWxzcHJzeGJ1ZWx1d3N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzODYwNjIsImV4cCI6MjA4Nzk2MjA2Mn0.Qgo_SyCWakbzZ_g4LAl2C3CRhFbETvFGofEkHZh_A4I'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54em1lZ3h2Z3psbGhrdmthY3p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzODg4MzIsImV4cCI6MjA4Nzk2NDgzMn0.-sRpnkAZB3FbCX9fgKX126r9xVioPdHwp86HI17F4uQ'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
